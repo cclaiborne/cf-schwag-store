2510299f1888606c91c92060c84134343f2771c3
sealocal <local.mat@gmail.com>
2014-02-27 13:05:13 -0800

    rails new cf_schwag_store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CfSchwagStore::Application.config.secret_key_base = '873a4c374ab492f73a63c78c3326a72c4ae815d7bc66efd5c8e970c00fd7cea131d2437155b1de67911791d3e97fcdc4d7253cfd8cbb4af78564386352c449ed'
