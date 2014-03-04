6a0612d112653dfad22acb662c27e9d7f7b99846
cclaiborne <claiborne206@gmail.com>
2014-03-04 15:15:51 -0800

    commiting to pull the most recent commit

--- test/features/can_visit_store_test.rb
+++ test/features/can_visit_store_test.rb
@@ -3,7 +3,5 @@ require "test_helper"
 feature "CanVisitStore" do
   scenario "the test is sound" do
     visit root_path
-    page.must_have_content "Hello World"
-    page.wont_have_content "Goobye All!"
   end
 end
