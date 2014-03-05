6d1ecb8c2d8beee4617dee92d957c0e2871275d7
cclaiborne <claiborne206@gmail.com>
2014-03-04 16:48:39 -0800

    add tests to add_test_branch

--- test/features/can_see_sku_number_test.rb
+++ test/features/can_see_sku_number_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 feature "CanSeeSkuNumber" do
   scenario "the test is sound" do
-    visit product_path(1)
+    #visit product_path(1)
     page.must_have_content "SKU"
 
   end
