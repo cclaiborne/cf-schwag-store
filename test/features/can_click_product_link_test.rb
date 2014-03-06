f1a63eabefb2366a92f780517cbe01986b6f7298
cclaiborne <claiborne206@gmail.com>
2014-03-06 11:46:37 -0800

    add tests

--- test/features/can_click_product_link_test.rb
+++ test/features/can_click_product_link_test.rb
@@ -2,8 +2,12 @@ require "test_helper"
 
 feature "CanClickProductLink" do
   scenario "the test is sound" do
+    catalogue = FactoryGirl.create(:category)
+    p1 = FactoryGirl.create(:product)
+    p1.product_category_id = catalogue.id
+    p1.save
     visit root_path
-    click_on("product")
+    click_on(p1.name)
     #add statement to confirm user is on product page
   end
 end
