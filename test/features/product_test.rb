9c1a9c49183a9f3c0d60dab6f771ae6241e58d3b
Jerrold Thompson <jet@whidbey.com>
2014-03-05 21:58:11 -0800

    style correction and test separation

--- test/features/product_test.rb
+++ test/features/product_test.rb
@@ -13,7 +13,13 @@ feature "Product Show Feature" do
     page.wont_have_content "Turtles all the Way Down!"
     # save_and_open_page
     click_link('ADD TO CART')
-    click_link('Checkout')
-
+  end
+  scenario "User clicks checkout button" do
+    catalogue = FactoryGirl.create(:category)
+    p1 = FactoryGirl.create(:product)
+    p1.product_category_id = catalogue.id
+    p1.save
+    visit product_path(p1.permalink )
+    click_link('CHECKOUT')
   end
 end
