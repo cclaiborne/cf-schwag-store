0008795a7f0a8a7a05b4b2ad00f35cfbb70fb2d5
sealocal <local.mat@gmail.com>
2014-03-13 01:09:07 -0700

    do not send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ CfSchwagStore::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'gmail.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
